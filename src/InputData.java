ac6465b921eb8cb0686aca41fd4cf2c99a1a63fb
manilovq@gmail.com <manilov>
2018-02-08 12:50:12 +0300

    initial commit

---
+++ src/InputData.java
@@ -0,0 +1,18 @@
+public class InputData {
+
+    String inputString;
+    String pattern;
+    public InputData(String inputString, String pattern) {
+        this.inputString = inputString;
+        this.pattern = pattern;
+    }
+
+    public String getInputString() {
+        return inputString;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+}
---
+++ src/WordCounter.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+import java.util.Scanner;
+
+public class WordCounter {
+    public static void main(String[] args) throws IOException{
+
+        InputData inString = readFile();
+
+        System.out.println(countWords(inString));
+
+
+    }
+
+    public static InputData readFile() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String[] inputString = scanner.nextLine().split("\\s");
+        String fileDir = inputString[0];
+        String pattern = inputString[1].toLowerCase();
+        scanner.close();
+        String str = "";
+        Scanner in = new Scanner(new File(fileDir));
+        while (in.hasNext()) {
+            str += in.nextLine() + "\r\n";
+        }
+        in.close();
+        return new InputData(str, pattern);
+
+    }
+
+    public static int countWords(InputData inputData) {
+        String str = inputData.getInputString();
+        String pattern = inputData.getPattern();
+        String[] words = str.toLowerCase().split("\\s");
+        int counter = 0;
+        for ( String word : words) {
+            if (word.equals(pattern))
+                counter++;
+        }
+        return counter;
+    }
+
+
+}
